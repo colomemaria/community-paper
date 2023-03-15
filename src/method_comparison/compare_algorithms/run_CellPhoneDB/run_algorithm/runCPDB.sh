319772ab6132daa7782b4a5e62bf858efc4efc5f
macelik <mcelik@master.cluster.local>
2023-03-15 11:24:28 +0100

    up

--- src/method_comparison/compare_algorithms/run_CellPhoneDB/run_algorithm/runCPDB.sh
+++ src/method_comparison/compare_algorithms/run_CellPhoneDB/run_algorithm/runCPDB.sh
@@ -1,10 +1,20 @@
+#conda activate cpdbnnet
+# Set the directory path to the directory containing the DEG samples
 samples_dir=../../../../../results/method_comparison/compare_algorithms/CPDB/samples_DEGs/
+
+# Get a list of unique variable names (i.e., sample names) by extracting the first part of the file names in the directory and removing duplicates
 my_vars=$(ls "$samples_dir" | cut -d_ -f1 | uniq)
-#output_dir=../../../../../results/method_comparison/compare_algorithms/CPDB/
+
+# Set the path to the custom database file
 custom_db=../../../../../results/method_comparison/build_customDB/CPDB/custom_cellphone.db
 
+# Loop over each sample variable name
 for sample in $my_vars;
 do
-mkdir ${samples_dir}${sample}_results; 
-cellphonedb method degs_analysis ${samples_dir}${sample}_meta.tsv ${samples_dir}${sample}_counts.tsv ${samples_dir}${sample}_DEGs.tsv --database $custom_db --counts-data hgnc_symbol --output-path ${samples_dir}${sample}_results/;
+  # Create a subdirectory for the sample results
+  mkdir ${samples_dir}${sample}_results;
+
+  # Run CellPhoneDB's DEG analysis method on the sample using the custom database, with input files in the sample directory and output files in the sample results subdirectory
+  cellphonedb method degs_analysis ${samples_dir}${sample}_meta.tsv ${samples_dir}${sample}_counts.tsv ${samples_dir}${sample}_DEGs.tsv --database $custom_db --counts-data hgnc_symbol --output-path ${samples_dir}${sample}_results/;
 done;
+
