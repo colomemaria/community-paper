bf9364316857131def76bcda3d39a5f48728b433
macelik <mcelik@master.cluster.local>
2023-08-09 11:08:57 +0200

    cpdb update

--- update_gitignore.sh
+++ update_gitignore.sh
@@ -1,1 +1,1 @@
-find . -size +110M | sed 's|^\./||g' | cat >> .gitignore; awk '!NF || !seen[$0]++' .gitignore
+find . -size +104M | sed 's|^\./||g' | cat >> .gitignore; awk '!NF || !seen[$0]++' .gitignore
