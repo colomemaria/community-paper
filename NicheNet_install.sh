5077ff40fc0bc3612ea6fb9e9f3a8d4e0350eb98
macelik <41537897+macelik@users.noreply.github.com>
2024-02-26 13:55:05 +0100

    Update NicheNet_install.sh

--- NicheNet_install.sh
+++ NicheNet_install.sh
@@ -9,7 +9,7 @@ source $CONDA_PATH/bin/activate NicheNet
 mamba env update --file NicheNet_env.yml
 R -e 'options(repos = c(CRAN = "https://cloud.r-project.org/")); install.packages("BiocManager"); BiocManager::install("metaboliteIDmapping"); q()'
 R -e 'devtools::install_github("satijalab/seurat-object@v4.1.3"); devtools::install_github("satijalab/seurat@v4.3.0"); q()';
-R -e 'devtools::install_github("saezlab/OmnipathR@v3.7.0"); devtools::install_github("saeyslab/nichenetr"); devtools::install_github("SoloveyMaria/community", upgrade = "always"); q()';
+R -e 'devtools::install_github("saezlab/OmnipathR"); devtools::install_github("saeyslab/nichenetr"); devtools::install_github("SoloveyMaria/community", upgrade = "always"); q()';
 R -e 'IRkernel::installspec(); q()';
 
 
